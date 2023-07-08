5fd3fd6b0beddad6c665a4c78d4ee25d240b36c5
nhuzjak20 <nhuzjak20@foi.hr>
2023-07-08 04:23:26 +0200

    Parsiranje radi, imam informacije od tijela

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(express.static(path.join(__dirname, 'html')))
 
 app.get('/', (req, res) => {
@@ -12,8 +14,17 @@ app.get('/promjeniNaslov', (req, res) => {
 })
 
 app.get('/ispit_navigacija', (req, res) => {
-    console.log("Radi Ispit navigacija")
+    //console.log("Radi Ispit navigacija")
     res.sendFile(__dirname + "/html/ispit-navigacija.html")
+})
+
+app.get('/IspitPrviZadatak', (req, res)=>{
+    res.sendFile(__dirname + "/html/templates/Ispit/prvi-zadatak/zadatak.html")
+})
+
+app.post('/RijesiPrviZadatak',urlencodedParser ,(req, res)=>{
+    //console.log("Radi")
+    console.log(req.body.PrviPrvi)
 })
 
 app.listen(5000, ()=>{
